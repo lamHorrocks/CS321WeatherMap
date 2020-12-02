12a2352f9867bdc3e7aac85110e391f34afba8ff
nat019 <71346900+nat019@users.noreply.github.com>
2020-12-02 13:46:55 -0600

    Updates the weather pane for each city

--- WeatherSystem/src/main/java/weathersystem/WeatherSystem.java
+++ WeatherSystem/src/main/java/weathersystem/WeatherSystem.java
@@ -37,32 +37,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.geometry.Insets; 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.TilePane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -82,7 +67,6 @@ public class WeatherSystem extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //launch(WeatherSystem.class, args);
         launch();
     }
     
@@ -90,11 +74,9 @@ public class WeatherSystem extends Application {
     public void start(Stage stage) throws IOException {
 
         // Use a border pane as the root for scene
-        BorderPane border = new BorderPane();
-        
         //set weather pane
-        GridPane weathergrid = addWeatherPane();
-        border.setRight(weathergrid);
+        setWeather(sCity);
+
         //set map pane
         border.setLeft(addCityPane(addMapPane()));
 
@@ -178,15 +160,14 @@ public class WeatherSystem extends Application {
         
         
         //On clicked functionality for the buttons
-        hamilton.setOnAction(e -> updateWeatherValues("Hamilton"));
-        florence.setOnAction(e -> updateWeatherValues("Florence"));
-        huntsville.setOnAction(e -> updateWeatherValues("Huntsville"));
-        decatur.setOnAction(e -> updateWeatherValues("Decatur"));        
-        scottsboro.setOnAction(e -> updateWeatherValues("Scottsboro"));
-        cullman.setOnAction(e -> updateWeatherValues("Cullman"));        
-        fortpayne.setOnAction(e -> updateWeatherValues("Fort Payne"));
-        gadsden.setOnAction(e -> updateWeatherValues("Gadsden"));
-        
+        hamilton.setOnAction(e -> setWeather("Hamilton"));
+        florence.setOnAction(e -> setWeather("Florence"));
+        huntsville.setOnAction(e -> setWeather("Huntsville"));
+        decatur.setOnAction(e -> setWeather("Decatur"));        
+        scottsboro.setOnAction(e -> setWeather("Scottsboro"));
+        cullman.setOnAction(e -> setWeather("Cullman"));        
+        fortpayne.setOnAction(e -> setWeather("Fort Payne"));
+        gadsden.setOnAction(e -> setWeather("Gadsden"));
         
         //Adds all of the buttons to the pane
         anchorpane.getChildren().addAll(pane,hb);
@@ -197,84 +178,87 @@ public class WeatherSystem extends Application {
  /**
  * Creates a weather pane for the map
  */
-    private GridPane addWeatherPane() throws IOException {
-
+    private void addWeatherPane() {
+        
         DateFormat dateFormat = new SimpleDateFormat("h:mm aa");
     	sTime = dateFormat.format(new Date());  
    
-        GridPane grid = new GridPane();
+        //GridPane grid = new GridPane();
         ColumnConstraints column1 = new ColumnConstraints();    
         column1.setPercentWidth(0);
 
-    
-        grid.getColumnConstraints().addAll(column1); // each get 50% of width
+    //Setting the padding  
+      weathergrid.setPadding(new Insets(10, 10, 10, 10)); 
+        weathergrid.getColumnConstraints().addAll(column1); // each get 50% of width
 
         // Category in column 1, row 1
         Text city = new Text(sCity + ", AL Weather");
         city.setFont(Font.font("Arial", FontWeight.BOLD, 15));
-        grid.add(city, 0, 0); 
+        weathergrid.add(city, 0, 0); 
         
         Text currentTime = new Text("as of " + sTime + " CST");
         currentTime.setFont(Font.font("Arial", FontWeight.LIGHT, 11));
-        grid.add(currentTime, 0, 1); 
+        weathergrid.add(currentTime, 0, 1); 
         
         Text currentTemp = new Text(sTemp + "\u00B0");
         currentTemp.setFont(Font.font("Arial", FontWeight.BOLD, 55));
-        grid.add(currentTemp, 0, 2, 2, 3); 
+        weathergrid.add(currentTemp, 0, 2, 2, 3); 
         
         Text currentDesc = new Text(sCondition);
         currentDesc.setFont(Font.font("Arial", FontWeight.BOLD, 15));
-        grid.add(currentDesc, 0, 5);
+        weathergrid.add(currentDesc, 0, 5);
          
-        Text currentForecast = new Text(sForecast + "\n");
+        Text currentForecast = new Text("\nToday's Forecast for \n" + sCity + ", AL\n");
         currentForecast.setFont(Font.font("Arial", FontWeight.BOLD, 15));
-        grid.add(currentForecast, 0, 9);
+        weathergrid.add(currentForecast, 0, 9);
         
-        Text currentTemps = new Text("Minimum Temperature\t\tMaximum Temperature\n" + minTemp + "\u00B0\t\t\t" 
-                + maxTemp + "\u00B0");
-        currentTemps.setFont(Font.font("Arial", FontWeight.BOLD, 11));
-        grid.add(currentTemps, 0, 10);
+        Text currentMin = new Text("Minimum Temperature\n" + minTemp + "\u00B0");
+        currentMin.setFont(Font.font("Arial", FontWeight.BOLD, 11));
+        weathergrid.add(currentMin, 0, 10);
         
-        // House icon in column 1, rows 1-2
-        String image_path = sImage;
-        try (InputStream stream = new FileInputStream(image_path)) {
-            ImageView imageHouse = new ImageView(new Image(stream));
-            grid.add(imageHouse, 1, 2, 2, 3);
-        }
-
-        grid.setGridLinesVisible(false);
+        Text currentMax = new Text("\nMaximum Temperature\n" + maxTemp + "\u00B0");
+        currentMax.setFont(Font.font("Arial", FontWeight.BOLD, 11));
+        weathergrid.add(currentMax, 0, 11);
         
-        return grid;
+        weathergrid.setGridLinesVisible(false);
+        
     }
     
-    private void updateWeatherValues(String cityName){
+    private void setWeather(String cityName){
         
         WeatherData cityData = parser.getCityForecast(cityName);
-        
+
         sCity = cityName;
         sTemp = cityData.getTemp();
         sCondition = cityData.getDescription();
         maxTemp = cityData.getTempMax();
         minTemp = cityData.getTempMin();
-        humidity = cityData.getHumidity();
-        clouds = cityData.getClouds();
-        windSpeed = cityData.getWindSpeed();
         
-        System.out.println(sCity + "\n" + sTemp + "\n" + sCondition + "\n" + maxTemp + "\n" + minTemp + "\n" + humidity + "\n" + clouds + "\n" + windSpeed + "\n");
+        weathergrid.getChildren().clear();
+        addWeatherPane();
+        border.setRight(weathergrid);
+
+        //System.out.println(sCity + "\n" + sTemp + "\n" + sCondition + "\n" + maxTemp + "\n" + minTemp + "\n" + humidity + "\n" + clouds + "\n" + windSpeed + "\n");
         
     }
-    
+
+    private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
+        for (Node node : gridPane.getChildren()) {
+            if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+                return node;
+            }
+        }
+    return null;
+    }
+
     private String sCity = "Huntsville";
     private String sTime = "";
     private double sTemp = 60;
     private String sCondition = "Clear";
-    private String sImage = "sunny.png";
-    private String sForecast = "Today's Forecast for \n" + sCity + ", AL";
     private double maxTemp = 0; 
     private double minTemp = 0;
-    private double humidity = 0;
-    private double clouds = 0;
-    private double windSpeed = 0;
-    private APIParser parser = new APIParser();
+    private final APIParser parser = new APIParser();
+    private final GridPane weathergrid = new GridPane();;
+    private final BorderPane border = new BorderPane();;
     
 }
